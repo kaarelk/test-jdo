3f174e944b8bd673c8f1bfa39c48aab3df04fe60
kaarelk <kaarelk@github>
2014-02-18 14:40:51 +0200

    sequence allocation size test

--- src/test/java/org/datanucleus/test/SimpleTest.java
+++ src/test/java/org/datanucleus/test/SimpleTest.java
@@ -1,11 +1,21 @@
 package org.datanucleus.test;
 
-import org.junit.*;
-import javax.jdo.*;
+import java.sql.Connection;
 
-import static org.junit.Assert.*;
-import mydomain.model.*;
+import java.sql.ResultSet;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
+import javax.jdo.annotations.SequenceStrategy;
+import javax.jdo.datastore.JDOConnection;
+import javax.jdo.datastore.Sequence;
+import javax.jdo.metadata.JDOMetadata;
+
 import org.datanucleus.util.NucleusLogger;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class SimpleTest
 {
@@ -14,20 +24,40 @@ public class SimpleTest
     {
         NucleusLogger.GENERAL.info(">> test START");
         PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("MyTest");
+        
 
+        
+        JDOMetadata md = pmf.newMetadata();
+        md.newPackageMetadata("test").newSequenceMetadata("userseq", SequenceStrategy.NONCONTIGUOUS).
+            setDatastoreSequence("users_seq").setAllocationSize(10);
+        pmf.registerMetadata(md);
+        
         PersistenceManager pm = pmf.getPersistenceManager();
         Transaction tx = pm.currentTransaction();
         try
         {
             tx.begin();
-
-            // [INSERT code here to persist object required for testing]
+            Sequence seq = pm.getSequence("userseq");
+            long l = seq.nextValue();
+            JDOConnection jdo = pm.getDataStoreConnection();
+            Connection con = (Connection) jdo.getNativeConnection();
+            
+            ResultSet rs = con.createStatement().executeQuery("select NEXT_VAL from SEQUENCE_TABLE where SEQUENCE_NAME = 'users_seq'");
+            rs.next();
+            long current = rs.getLong(1);
+            rs.close();
+            jdo.close();
+            if( current != 10L) {
+                fail("Current seq = " + current);
+            }
+            
             tx.commit();
         }
         catch (Throwable thr)
         {
             NucleusLogger.GENERAL.error(">> Exception thrown persisting data", thr);
-            fail("Failed to persist data : " + thr.getMessage());
+            thr.printStackTrace();
+            fail( "" + thr);
         }
         finally 
         {
